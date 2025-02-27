aa4bacdebaf01652eaceb7a098a4af98cbc06783
Amanda Cornelsen <116924766+AmandaCornelsen@users.noreply.github.com>
2025-02-27 20:53:12 -0300

    Add files via upload

---
+++ ProjetoEx_JUnitCalc/src/Produto.java
@@ -0,0 +1,16 @@
+
+public class Produto {
+
+	    String codigo; 
+	    int preco; 
+
+	    public Produto(String id, int valor) { 
+
+	        this.codigo = id; 
+
+	        this.preco = valor; 
+
+	    } 
+
+	} 
+	
